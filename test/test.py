123ce0074e066727c0ba116ab03966bb81d30f1b
Ale1188 <dramoizta@gmail.com>
2024-06-20 19:53:38 -0700

    Assignment 2

--- test/test.py
+++ test/test.py
@@ -41,3 +41,32 @@ print(me)
 # get the value
 print(me["firstName"])
 
+
+#create a calculador using functions
+def printMenu():
+    print("1) Sum")
+    print("2) Sub")
+    print("3) Div")
+    print("4) Multi")
+
+# plain intructions
+printMenu()
+opt = input("Select the option: ")
+number1 = float(input("please give me the first number: "))
+number2 = float(input("please give me the second number: "))
+
+if opt == "1":
+    total = number1 + number2
+    print("Total: " + str(total))
+elif opt == "2":
+    total = number1 - number2
+    print(f"Total: {total}")
+elif opt == "3":
+    total = number1 / number2
+    print(f"Total: {total}")
+elif opt == "4":
+    if number2 == 0:
+        print("You cannot divide by zero")
+    else:
+        total = number1 * number2
+        print(f"Total: {total}")
